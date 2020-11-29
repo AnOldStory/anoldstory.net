0f9d07f35ff96d34d22ac97756561ac235096da8
AnOldStory <hc9904@hanyang.ac.kr>
2020-11-29 08:08:39 +0000

    menu design

--- frontend/src/route/Router.js
+++ frontend/src/route/Router.js
@@ -5,18 +5,28 @@ import AdminRouter from "./AdminRouter";
 import PublicRouter from "./PublicRouter";
 
 class Router extends Component {
+  handleFooter(e) {
+    console.log(e.key);
+    if (e.key === "d") {
+      window.location.replace("/admin");
+    }
+  }
   render() {
     return (
       <BrowserRouter basename="/">
-        <>
-          <Switch>
-            <Route
-              path="/admin"
-              render={(props) => <AdminRouter {...props} />}
-            />
-            <Route path="/" render={(props) => <PublicRouter {...props} />} />
-          </Switch>
-        </>
+        <Switch>
+          <Route path="/admin" render={(props) => <AdminRouter {...props} />} />
+          <Route path="/" render={(props) => <PublicRouter {...props} />} />
+        </Switch>
+        <div
+          type="hidden"
+          className="footer"
+          ref={(i) => {
+            i.focus();
+          }}
+          tabIndex="1"
+          onKeyDown={this.handleFooter}
+        />
       </BrowserRouter>
     );
   }
